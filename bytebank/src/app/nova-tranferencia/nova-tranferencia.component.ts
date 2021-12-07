89aa8d10232f4b8cbddf56b991791bd731fec10a
Lucas Duarte <lucasduartedev@gmail.com>
2021-12-07 10:39:29 -0300

    Curso: Começando com angular - Capitulo 2

--- bytebank/src/app/nova-tranferencia/nova-tranferencia.component.ts
+++ bytebank/src/app/nova-tranferencia/nova-tranferencia.component.ts
@@ -1,12 +1,18 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
 @Component({
-    selector:'app-nova-transferencia',
-    templateUrl: './nova-transferencia.component.html',
-    styleUrls: ['./nova-tranferencia.component.scss']
+  selector: 'app-nova-transferencia',
+  templateUrl: './nova-transferencia.component.html',
+  styleUrls: ['./nova-tranferencia.component.scss'],
 })
 export class NovaTranferenciaCoponent {
 
+  valor: number = 0;
+  destino: number = 153;
 
-
+  transferir() {
+    console.log(' ===== Botao transferir! ===== ');
+    console.log(`Valor: ${this.valor}`)
+    console.log(`Destino: ${this.destino}`)
+  }
 }
