96111e7f685464237d9ad11467694cee073c6a8c
Lucas Duarte <lucasduartedev@gmail.com>
2021-12-07 10:00:53 -0300

    Curso: Começando com angular - Capitulo 1

--- bytebank/src/app/app.module.ts
+++ bytebank/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { NovaTranferenciaCoponent } from './nova-tranferencia/nova-tranferencia.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -5,7 +6,8 @@ import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NovaTranferenciaCoponent
   ],
   imports: [
     BrowserModule
---
+++ bytebank/src/app/nova-tranferencia/nova-tranferencia.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector:'app-nova-transferencia',
+    templateUrl: './nova-transferencia.component.html',
+    styleUrls: ['./nova-tranferencia.component.scss']
+})
+export class NovaTranferenciaCoponent {
+
+
+
+}
